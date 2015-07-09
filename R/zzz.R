96da900bb7413116a5d092bd50fad6d544155be7
Scott Chamberlain <myrmecocystus@gmail.com>
2015-07-08 17:51:04 -0700

    tidied some code

--- R/zzz.R
+++ R/zzz.R
@@ -1,28 +1,28 @@
-etcd_GET <- function(url, args, ...){
-  res <- GET(url, query=args, ...)
+etcd_GET <- function(url, args, ...) {
+  res <- GET(url, query = args, ...)
   stop_for_status(res)
   content(res, "text")
 }
 
 etcd_PUT <- function(url, value, ttl=NULL, dir=FALSE, ...){
-  if(missing(value)){
-    res <- PUT(url, query=list(dir=TRUE), ...)
+  if (missing(value)) {
+    res <- PUT(url, query = list(dir = TRUE), ...)
   } else {
-    res <- PUT(url, body = list(value=value), query=etc(list(ttl=ttl, dir=dir)), encode="form", ...)
+    res <- PUT(url, body = list(value = value), query = etc(list(ttl = ttl, dir = dir)), encode = "form", ...)
   }
   stop_for_status(res)
   content(res, "text")
 }
 
-etcd_POST <- function(url, value, ttl=NULL, ...){
-  res <- POST(url, body = list(value=value), query=etc(list(ttl=ttl)), encode="form", ...)
+etcd_POST <- function(url, value, ttl=NULL, ...) {
+  res <- POST(url, body = list(value = value), query = etc(list(ttl = ttl)), encode = "form", ...)
   stop_for_status(res)
   content(res, "text")
 }
 
 etcd_DELETE <- function(url, args, ...){
-  res <- DELETE(url, query=args, ...)
-  if(res$status_code > 201){
+  res <- DELETE(url, query = args, ...)
+  if (res$status_code > 201) {
     warn_for_status(res)
     content(res)
   }
@@ -35,4 +35,4 @@ etcd_parse <- function(x, simplify=FALSE){
 
 etcdbase <- function() "http://127.0.0.1:4001/v2/"
 
-etc <- function (l) Filter(Negate(is.null), l)
+etc <- function(l) Filter(Negate(is.null), l)
