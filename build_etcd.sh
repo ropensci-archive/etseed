58aa87e56bdb00f600111616fd308ccb2730af54
Scott Chamberlain <myrmecocystus@gmail.com>
2015-07-21 15:22:13 -0700

    trying a fix for etcd install

--- build_etcd.sh
+++ build_etcd.sh
@@ -14,8 +14,4 @@ cd etcd
 git checkout $ETCD_VERSION
 ./build
 
-
-${TRAVIS:?"This is not a Travis build. All Done"}
-#Temporal solution to travis issue #155
-sudo rm -rf /dev/shm && sudo ln -s /run/shm /dev/shm
 echo "All Done"
