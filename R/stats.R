399380f6c6c05ec9a051989b08b7db6bddfb316f
Scott Chamberlain <myrmecocystus@gmail.com>
2015-08-27 16:20:05 -0700

    bump minor version, added stats fxn, fix #10

---
+++ R/stats.R
@@ -0,0 +1,27 @@
+#' Get etcd statistics
+#'
+#' @export
+#'
+#' @param which (character) one of leader (default) or self
+#' @param pretty (logical) parse text to an R list, or not. Default: \code{TRUE}
+#' @param ... Further args passed on to \code{\link[httr]{GET}}
+#' @return Prints a human readable text representation to console.
+#' @examples \dontrun{
+#' # leader stats
+#' stats()
+#'
+#' # self stats
+#' stats("self")
+#'
+#' # Prety or not
+#' stats(pretty = TRUE)
+#' stats(pretty = FALSE)
+#' }
+stats <- function(which = "leader", pretty = TRUE, ...) {
+  res <- etcd_GET(paste0(etcdbase(), "stats/", which), NULL, ...)
+  if (pretty) {
+    jsonlite::fromJSON(res)
+  } else {
+    res
+  }
+}
